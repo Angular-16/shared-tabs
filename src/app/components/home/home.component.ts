387ef954657444e889d88ca780a267adfd6ff47b
Valeriy Semenenko <gearmobile@gmail.com>
2025-01-31 16:14:00 +0300

    feat: :sparkles: add change color behavior

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'st-home',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
+  toggle = signal(false);
 
+  onClick(): void {
+    this.toggle.update((v) => !v);
+  }
 }
